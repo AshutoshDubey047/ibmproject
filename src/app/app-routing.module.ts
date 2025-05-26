ab14111f62f8322363e2d4a7be06f9cfead697b6
PRATAP <47ashutoshdubey@gmail.com>
2025-05-26 13:11:00 +0530

    My update of monday

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { AppHome } from './home/home.component'
+import { AboutusComponent} from './aboutus/aboutus.component'
+const routes: Routes = [
+  {path : 'home', component:AppHome},
+  {path : 'about', component:AboutusComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
