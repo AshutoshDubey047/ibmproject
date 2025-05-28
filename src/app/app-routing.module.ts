3cf79d541b083d20b5a303f43244bb6ceab86f92
PRATAP <47ashutoshdubey@gmail.com>
2025-05-28 16:18:36 +0530

    update by routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppHome } from './home/home.component'
 import { AboutusComponent} from './aboutus/aboutus.component'
+import { ServicesComponent } from './services/services.component'
+import { ErrorPageComponent } from './error-page/error-page.component'
 const routes: Routes = [
+  {path : '', redirectTo: 'home', pathMatch: 'full'},
   {path : 'home', component:AppHome},
-  {path : 'about', component:AboutusComponent}
+  {path : 'about', component:AboutusComponent},
+  {path : 'services', component:ServicesComponent},
+  // {path : 'error', component:ErrorPageComponent},
+  // {path : '**', redirectTo:'error'}
+  {path:'**',component:ErrorPageComponent}
 ];
 
 @NgModule({
